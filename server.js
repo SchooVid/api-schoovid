59455c034be0d50528858bd49138cfbaf82b9162
tran.nicolas <tnicolas28@gmail.com>
2021-04-12 13:58:21 -0700

    ajout route pas encore codée, connexion à la bdd distante ok

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
-const mysql   = require('mysql');
+const bodyparser = require('body-parser');
+const mysql  = require('mysql');
 
 const app     = express();
 const port    = process.env.PORT || 3000;
 
+const con = mysql.createConnection({
+    host : '51.178.139.94',
+    user : "nixo",
+    password : "nixobdd1*",
+    database : 'schoovid'
+});
+
 
 app.get('/', (req,res) => {
-    res.send("Welcome to Schoovid API!");
-})
+
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        res.send("Welcome to Schoovid API");
+    });
+
+});
+
+app.post('/signup',(req,res) => {
+
+});
+
+app.post('/signin', (req,res) => {
+
+});
+
+
 
 app.listen(port,() => {
     console.log(`Server listening on port : ${port}`);
