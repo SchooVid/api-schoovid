66655496a756d62d51ce251abc82fc0a96552de7
tran.nicolas <tran.nicolas28@gmail.com>
2021-07-16 09:35:37 -0700

    Added subroute for proposed courses

--- src/index.route.ts
+++ src/index.route.ts
@@ -6,6 +6,7 @@ import { course_category_router } from "./routes/cours_category.route";
 import { course_level_router } from "./routes/course_level.route";
 import { course_participant_router } from "./routes/course_participant.route";
 import { message_router} from "./routes/message.route";
+import { proposed_course_router} from "./routes/proposed_course.route"
 
 
 
@@ -18,6 +19,7 @@ router.use('/course_level',course_level_router);
 router.use('/course_category',course_category_router);
 router.use('/course_participant',course_participant_router);
 router.use('/message',message_router);
+router.use('/proposed_course',proposed_course_router)
 
 
 
